5c5b24cc706cf7ea6a48b95fa452e5f09ec1d014
songseonghyun <57611077+songseonghyun@users.noreply.github.com>
2022-02-16 01:58:28 +0900

    Add files via upload

--- portfolio/main.js
+++ portfolio/main.js
@@ -41,38 +41,86 @@ window.addEventListener("scroll", reveal);
 
 
 
-const introduce = document.getElementsByClassName('IntroducePage');
-const leftbtn = document.getElementById('leftbtn');
-const rightbtn = document.getElementById('rightbtn');
-const show = document.getElementsByClassName('show')
-let place = 0;
+let projectname = ['web', 'java', 'oracle', 'python'];
 
-leftbtn.onclick = function() {
-  place += 500;
-  if (place > 0) {
-    // console.log('check');
-    place = -1500;
-    for(let j=0; j<introduce.length; j++) {
-      introduce[j].style.transform = 'translateY(0px);'
-    }
-  }
-  for (let i=0; i<introduce.length; i++) {
-    introduce[i].style.transform = 'translateY('+place+'px)';
-  }
-};
+// for (let i=1; i<projectname.length+1; i++) {
+//   $('#Project .text-box:nth-child('+ i + ')').click(function() {
+//     $('#'+projectname[i-1]+'project').css('opacity', 1).css('z-index', 1000);
+//     return false;
+//   });
+
+//   $('#'+ projectname[i-1] +'project').click(function() {
+//     $(this).css('opacity', 0).css('z-index', -1000);
+//   });
+  
+//   $('#'+ projectname[i-1] +'project .projcontainer').click(function() {
+//     // $(this).css('border-radius', '50%')
+//     return false;
+//   });
+// }
+  
+for (let i=1; i<projectname.length; i++) {
+
+  $('#Project .text-box:nth-child(' + i + ')').click(function() {
+    // location.href='./'+projectname[i-1]+'/'+projectname[i-1]+'projectbig.html'
+    window.open('https://github.com/songseonghyun/'+ projectname[i-1] +'Project');
+  });
+
+}
+
+$('#Project .text-box:nth-child(4)').click(function() {
+  // location.href='./'+projectname[i-1]+'/'+projectname[i-1]+'projectbig.html'
+  window.open('https://songseonghyun.github.io/StevenSeongHyun.github.io/');
+});
 
 
 
-rightbtn.onclick = function() {
-  place -= 500;
-  if (place <= -2000) {
-    // console.log('check');
-    place = 000;
-    for(let j=0; j<introduce.length; j++) {
-      introduce[j].style.transform = 'translateY(0px);'
-    }
-  }
-  for (let i=0; i<introduce.length; i++) {
-    introduce[i].style.transform = 'translateY('+place+'px)';
-  }
-};
+
+
+
+
+
+
+
+
+
+// const introduce = document.getElementsByClassName('IntroducePage');
+// const leftbtn = document.getElementById('leftbtn');
+// const rightbtn = document.getElementById('rightbtn');
+// const show = document.getElementsByClassName('show')
+// let place = 0;
+
+
+// leftbtn.onclick = function() {
+//   place += 500;
+//   if (place > 0) {
+//     // console.log('check');
+//     place = -1500;
+//     for(let j=0; j<introduce.length; j++) {
+//       introduce[j].style.transform = 'translateY(0px);'
+//     }
+//   }
+//   for (let i=0; i<introduce.length; i++) {
+//     introduce[i].style.transform = 'translateY('+place+'px)';
+//   }
+// };
+
+
+
+// rightbtn.onclick = function() {
+//   place -= 500;
+//   if (place <= -2000) {
+//     // console.log('check');
+//     place = 000;
+//     for(let j=0; j<introduce.length; j++) {
+//       introduce[j].style.transform = 'translateY(0px);'
+//     }
+//   }
+//   for (let i=0; i<introduce.length; i++) {
+//     introduce[i].style.transform = 'translateY('+place+'px)';
+//   }
+// };
+
+
+
+// const project = document.getElementsByClassName("project");
